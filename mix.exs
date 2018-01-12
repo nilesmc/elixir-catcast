0008515db50453b62c3d25cade3ce526c76ee636
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-01-11 16:13:52 -0800

    adding in google auth dep and controller

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Catcasts.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ueberauth, "~> 0.4"}, # for google auth
+      {:ueberauth_google, "~> 0.5"} # for google auth
     ]
   end
 
